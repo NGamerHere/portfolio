7bb62d36667e9636756b652d619c1b9b46e5f4cc
datta <codingdatta@gmail.com>
2024-02-25 13:31:44 +0530

    adjusted the height of the main div in the Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
       <div>
           <Nav/>
-          <div className="flex flex-col items-center justify-center h-[40rem]  ">
+          <div className="flex flex-col items-center justify-center h-[60rem]  ">
 
               <TypewriterEffectSmooth words={words}/>
               <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base  ">
